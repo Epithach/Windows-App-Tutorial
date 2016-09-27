28abae8552372d692bcabc51de05d1092f8f643e
Epithach <antoine.thach@gmail.com>
2016-09-27 17:36:42 +0200

    Starting a new MVVM Test

---
+++ Yolo/Yolo/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Yolo.ViewModels;
+
+// Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Yolo
+{
+    /// <summary>
+    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.DataContext = new MainViewModel();
+        }
+
+        public string FirstName
+        {
+            get { return this.FirstName; }
+            set
+            {
+                this.FirstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get { return this.LastName; }
+            set
+            {
+                this.LastName = value;
+            }
+        }
+
+        public string Email
+        {
+            get { return this.Email; }
+            set
+            {
+                this.Email = value;
+            }
+        }
+
+        private string Info;
+
+        private void Button_Click(Object sender, RoutedEventArgs e)
+        {
+            var dc = DataContext as MainViewModel;
+            dc.TextYolo = "test";
+            
+        }
+    }
+}
---
+++ Yolo/Yolo/ViewModels/MainViewModel.cs
@@ -0,0 +1,74 @@
+﻿using DomoThink.MVVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+/*--------------------------------------------------------
+ * MainViewModel.cs
+ * 
+ * Version: 1.0
+ * Author: Filipe
+ * Created: 23/09/2016 15:15:29
+ * 
+ * Notes:
+ * -------------------------------------------------------*/
+
+namespace Yolo.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        #region FIELDS
+
+
+
+        #endregion
+
+        #region PROPERTIES
+
+        private string text;
+        public string TextYolo
+        {
+            get { return this.text; }
+            set
+            {
+                this.text = value;
+                this.OnPropertyChanged("TextYolo");
+            }
+        }
+
+        private ICommand onButtonClick;
+        public ICommand OnButtonClick
+        {
+            get { return this.onButtonClick; }
+            set
+            {
+                this.onButtonClick = value;
+                this.OnPropertyChanged("OnButtonClick");
+            }
+        }
+
+        #endregion
+
+        #region CONSTRUCTORS
+
+        public MainViewModel()
+        {
+            //this.TextYolo = "Hello world!";
+            this.OnButtonClick = new Command(this.OnButtonAction);
+        }
+
+        #endregion
+
+        #region METHODS
+
+        private void OnButtonAction(object param)
+        {
+            this.TextYolo = "Hello world!";
+        }
+
+        #endregion
+    }
+}
